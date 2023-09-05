bdae4a3c0f17114f3bcb89e10e340233f2f13269
Natique Ibrar <72468182+Natique1415@users.noreply.github.com>
2023-09-05 22:21:51 +0400

    Update database_operations.py

--- database_operations.py
+++ database_operations.py
@@ -1,59 +1,36 @@
 import mysql.connector
-from mysql.connector import errorcode
 from rich.prompt import Prompt
 import re
 import sys
 from tabulate import tabulate
 
 
+
 config = {
     "host": "localhost",
     "user": "root",
     "password": "root",
-    "database": "contacts",
+    "database":"contacts"
 }
 
 
-def manage_db(config):
-    global db
-    try:
-        db = mysql.connector.connect(**config)
-    except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_BAD_DB_ERROR:
-            try:
-                db = mysql.connector.connect(
-                    **{key: value for key, value in config.items() if key != "database"}
-                )
-                cursor = db.cursor()
-                cursor.execute(f'CREATE DATABASE IF NOT EXISTS {config["database"]};')
-                db.commit()
-                cursor.close()
-            except mysql.connector.Error as create_err:
-                sys.exit(f"An error occurred while creating the 'contacts' database: {create_err}")
-        else:
-            sys.exit(f"Error {err}")
-    finally:
-        if "db" in locals() and db.is_connected():
-            ...
+# Initialize Database and table
+db = mysql.connector.connect(**{key: value for key, value in config.items() if key != "database"})
+cursor = db.cursor()
+cursor.execute(f'CREATE DATABASE IF NOT EXISTS {config["database"]};')
+cursor.execute(f"USE {config['database']};")
+cursor.execute(
+    """
+    CREATE TABLE IF NOT EXISTS user_data(
+        name TEXT,
+        phone TEXT,
+        category TEXT 
+    );            
+    """
+)
+db.commit()
+cursor.close()
 
-
-def create_table(config):
-    cursor = db.cursor()
-    cursor.execute(f"USE {config['database']};")
-    cursor.execute(
-        """
-        CREATE TABLE IF NOT EXISTS user_data(
-            name TEXT,
-            phone TEXT,
-            category TEXT 
-        );            
-        """
-    )
-    db.commit()
-    cursor.close()
-
-def close_server():
-    db.close()
 
 
 def check_table_empty():
@@ -74,34 +51,33 @@ def check_name_exist(name: str):
         cursor.close()
 
 
-# Needs to run
-manage_db(config)
-create_table(config)
-
-
 def add_contacts():
     cursor = db.cursor()
-    cursor.execute(f"USE {config['database']};")
-    db.commit()
+    cursor.execute(f"USE contacts;")
+
     print("")
+
     name = input("Name : ").strip().title()
     if re.search(r"\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+", name):
         ...
     else:
         sys.exit("Invalid Name,Try Again Later!")
     print("")
+
     country_code = Prompt.ask("Country Code", default="+971").strip()
     if re.search(r"^\+?(\d+)", country_code):
         ...
     else:
         sys.exit("Invalid Country Code,Try Again Later!")
     print("")
+
     phone_number = Prompt.ask("Phone Number", default="0000000000").strip()
     if re.search(r"^[0-9\s\-()]+$", phone_number):
         ...
     else:
         sys.exit("Invalid Phone Number,Try Again Later!")
     print("")
+
     category = Prompt.ask("Category", default="Friend").strip().title()
     if re.search(r"^[A-Za-z]+$", category):
         ...
@@ -124,19 +100,21 @@ def add_contacts():
 
 
 def remove_contacts():
-    cursor = db.cursor()
     print("")
     name = input("Name (to remove): ").strip().title()
     if re.search(r"\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+", name):
-        if check_table_empty() is None:
-            print("Table is Currently Empty")
-            print("")
+        cursor = db.cursor()
+        cursor.execute(f"SELECT * FROM user_data WHERE name = '{name}'")
+        result = cursor.fetchall()
+        cursor.close()
 
-        elif check_name_exist(name) == []:
+
+        if result == []:
             print("Name You Wanted to Delete, Does Not Exist in the first place")
             print("")
 
         else:
+            cursor = db.cursor()
             cursor.execute(f"DELETE FROM user_data WHERE name = '{name}';")
             db.commit()
             cursor.close()
@@ -144,33 +122,31 @@ def remove_contacts():
             print("Entered Name Deleted Successfully!")
             print("")
     else:
+        print("")
         sys.exit("Invalid Name,Try Again Later!")
 
 
 def search_contacts():
-    cursor = db.cursor()
     print("")
     name = input("Name (to search for): ").strip().title()
     if re.search(r"\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+", name):
-        cursor.execute(f'SELECT * FROM user_data WHERE name = "{name}"')
-        result_name = cursor.fetchall()
+        cursor = db.cursor()
+        cursor.execute(f"SELECT * FROM user_data WHERE name = '{name}'")
+        result = cursor.fetchall()
+        cursor.close()
 
-        if check_table_empty() is None:
-            print("Table is Currently Empty")
-            cursor.close()
-
-        elif result_name == []:
+        if result == []:
             print("Name You Wanted to Search, Does Not Exist in the first place")
-            cursor.close()
+            print("")
 
         else:
             print("")
             print(
                 tabulate(
                     {
-                        "Name": [result_name[0][0]],
-                        "Phone Number": [result_name[0][1]],
-                        "Category": [result_name[0][-1]],
+                        "Name": [result[0][0]],
+                        "Phone Number": [result[0][1]],
+                        "Category": [result[0][-1]],
                     },
                     headers="keys",
                     tablefmt="grid",
@@ -178,26 +154,24 @@ def search_contacts():
                 )
             )
             print("")
-            cursor.close()
 
     else:
+        print("")
         sys.exit("Invalid Name,Try Again Later!")
 
 
 def update_number():
-    cursor = db.cursor()
     print("")
     name = input("Name (to update for): ").strip().title()
     if re.search(r"\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+", name):
-        if check_table_empty() is None:
-            print("Table is Currently Empty")
-            print("")
-            cursor.close()
+        cursor = db.cursor()
+        cursor.execute(f"SELECT * FROM user_data WHERE name = '{name}'")
+        result = cursor.fetchall()
+        cursor.close()
 
-        elif check_name_exist(name) == []:
+        if result == []:
             print("Name You Wanted to Update, Does Not Exist in the first place")
             print("")
-            cursor.close()
 
         else:
             print("")
@@ -207,37 +181,49 @@ def update_number():
             else:
                 sys.exit("Invalid Country Code,Try Again Later!")
             print("")
+
             phone_number = Prompt.ask("Phone Number", default="0000000000").strip()
             if re.search(r"^[0-9\s\-()]+$", phone_number):
                 ...
             else:
                 sys.exit("Invalid Phone Number,Try Again Later!")
 
-            phone_number = f"{country_code} {phone_number}"
 
+            # Phone number
+            phone_number = f"{country_code} {phone_number}"
+            
+            cursor = db.cursor()
             cursor.execute(
                 f'UPDATE user_data SET phone = "{phone_number}" WHERE name = "{name}";'
             )
-            print("Update Executed")
-            print("")
             db.commit()
             cursor.close()
+            print("Update Executed")
+            print("")
+    else:
+        print("")
+        sys.exit("Invalid Name,Try Again Later!")
 
 
 def show_all():
+    #Checking if table is empty
     cursor = db.cursor()
-    name = []
-    phone = []
-    category = []
+    cursor.execute(f"SELECT * FROM user_data")
+    result = cursor.fetchall()
+    cursor.close()
 
-    if check_table_empty() is None:
+    if result == []:
         print("")
         print("Table is Currently Empty,Nothing to Display")
         print("")
 
+
+    # Display data (if table not empty)
     else:
-        cursor.execute("SELECT * FROM user_data;")
-        name, phone, category = zip(*cursor.fetchall())
+        name = []
+        phone = []
+        category = []
+        name, phone, category = zip(*result)
         print("")
         print(
             tabulate(
@@ -248,8 +234,10 @@ def show_all():
             )
         )
         print("")
-        cursor.close()
 
+
+def close_server():
+    db.close()
 
 if __name__ == "__main__":
     add_contacts()
